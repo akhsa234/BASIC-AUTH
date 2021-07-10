f4476460df4e02aaf9bb10dbd984876b14028ff0
akhsas234 <akhsas234@gmail.com>
2021-07-10 11:03:18 +0430

    Initial comment

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo.student;
+
+import java.util.Objects;
+
+public class Student {
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(studentId, student.studentId) &&
+                Objects.equals(studentName, student.studentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, studentName);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+
+    private final List<Student> students = Arrays.asList(
+            new Student(1, "hana stuarts"),
+            new Student(2, "sara eeeee"),
+            new Student(3, "sahar wwwww"),
+            new Student(4, "reza tttt")
+    );
+
+
+
+@GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable Integer studentId)  {
+    return students.stream()
+            .filter(student -> studentId.equals(student.getStudentId()))
+            .findFirst()
+            .orElseThrow(()-> new IllegalStateException("Student " + studentId + "does not exist"));
+
+    }
+}
